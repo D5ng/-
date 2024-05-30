4c756baf9965bd362e6d34288b601b134cf2258f
d5ng <dong5ffice@gmail.com>
2024-05-30 12:16:23 +0900

    Feat: 투두 헤더 UI 작업

--- src/components/todo/header/todo-header.tsx
+++ src/components/todo/header/todo-header.tsx
@@ -1,5 +1,19 @@
 import React from "react"
+import classes from "./todo-header.module.scss"
 
 export default function TodoHeader() {
-  return <div>TodoHeader</div>
+  return (
+    <header className={classes["todo-header"]}>
+      <div className={classes["todo-inner-header"]}>
+        <h1 className={classes["todo-header__date"]}>
+          TuesDay,
+          <span>
+            31<sup>st</sup>
+          </span>
+        </h1>
+        <span className={classes["todo-header__month"]}>Date</span>
+        <span className={classes["todo-header-task-count"]}>3 Tasks</span>
+      </div>
+    </header>
+  )
 }
